6732f0d4bb63788661fad4aae73ad45d358a1532
Mario Palato Villa CLOUD <mario.palato@compucloud.com.mx>
2022-08-17 13:16:48 -0500

    Código completado y funcionando al día actual 17/08/2022

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "CustomerHandler": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "apif6fc4817": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
